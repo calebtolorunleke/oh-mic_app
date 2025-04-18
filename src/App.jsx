ef49417b41f50a88f1c3384bb863edc8e02965c7
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-04-18 22:06:42 +0100

    techyjaunt on react proj

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Greeting from './Greeting'
+
+function App() {
+
+  return (
+    <div>
+    <Greeting /> 
+  </div>
+   
+  )
+}
+
+export default App
---
+++ src/Greeting.jsx
@@ -0,0 +1,12 @@
+import './Greeting.css'
+
+function Greeting(){
+    return (
+        <div>
+            <h1 className="greeting-h1">welcome to react</h1>
+            <p className="greeting-p">react is powerful</p>
+        </div>
+    )
+}
+
+export default Greeting
